8b0793b08001a86361be50a51c57c9a21c7970d2
Olena Openko <lenka.openko@gmail.com>
2026-07-09 15:57:31 +0200

    Created controller tests

---
+++ backend/src/test/java/org/olena/jobsearchtracker/common/controller/SpaControllerTest.java
@@ -0,0 +1,7 @@
+package org.olena.jobsearchtracker.common.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpaControllerTest {
+
+}
---
+++ backend/src/test/java/org/olena/jobsearchtracker/company/controller/CompanyControllerTest.java
@@ -0,0 +1,7 @@
+package org.olena.jobsearchtracker.company.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyControllerTest {
+
+}
